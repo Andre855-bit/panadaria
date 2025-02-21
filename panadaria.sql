3c1b9c18ab18c9f4b1cc634c44f15a5c7e6345d1
Andre855-bit <andremachado855@gmail.com>
2025-02-21 20:15:13 -0300

    Salvargit add .!

--- panadaria.sql
+++ panadaria.sql
@@ -10,23 +10,18 @@ create database dbpanadaria;
 
 use dbpanadaria;
 
-create table tbgenero(
-cod_gen int not null,
-nome_gen varchar(100),
-primary key(cod_gen));
-
 
 create table tbfuncionario(
 cod_fun int not null auto_increment,
 nome_fun varchar(50) not null,
-email_fun varchar(50),
+email_fun varchar(50) unique,
 tel_fun char(10),
 dataNasc_fun datetime,
 salario_fun decimal (9,2) default 0 check(salario_fun >= 0),
 sexo_fun char(1) default "X" check(sexo_fun in("F","M")),
-cod_gen int not null,
-primary key (cod_fun),
-foreign key (cod_gen) references tbgenero(cod_gen));
+primary key (cod_fun));
+
+
 
 create table tbusuarios(
 cod_usu int not null auto_increment,
@@ -43,6 +38,8 @@ email_forn varchar(30),
 tel_forn char(10),
 primary key(cod_forn));
 
+
+
 create table tbProdutos(
 cod_prod int not null auto_increment,
 desc_prod varchar(100),
@@ -58,8 +55,10 @@ create table tbCliente(
 cod_clie int not null auto_increment,
 nome varchar(100) not null,
 cpf_clie char(14) not null unique,
-email_clie varchar(100),
+email_clie varchar(100) unique,
 primary key(cod_clie));
+
+
 
 create table tbVendas(
 cod_ven int not null auto_increment,
@@ -85,3 +84,18 @@ desc tbFornecedor;
 desc tbProdutos;
 desc tbCliente;
 desc tbVendas;
+
+--Inserindo registros na tabelas
+
+insert into tbCliente(nome,cpf_clie,email_clie) values ('Marcos','213.456.879-12','exemplo@exemplo.com');
+insert into tbCliente(nome,cpf_clie,email_clie) values ('Tocatins','173.455.875-12','estado@exemplo.com');
+
+insert into tbFornecedor (nome_forn,email_forn,tel_forn) values('Verduras SA','verduras@exemplo.com','97586-9586');
+
+insert into tbfuncionario(nome_fun,email_fun,tel_fun,dataNasc_fun,salario_fun,sexo_fun) values ('Rainundo','rainundo@exemplo.com','45564-6124','1987-02-14',1707.21,'M');
+
+--Visualizando os registros nas tabelas
+
+select * from tbCliente;
+select * from tbFornecedor;
+select * from tbfuncionario;
